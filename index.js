6029b4dfc18416f77f83fd25ff90bdcb782eeb12
wptechprodigy <waheed.freelancer16@gmail.com>
2019-12-11 19:04:14 +0100

    setup db connection with server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config');
 
 // Type defintions
 const typeDefs = gql`
@@ -21,6 +24,13 @@ const server = new ApolloServer({
   resolvers
 });
 
-// On what port would we be listening to the server
-server.listen({ port: 8005 })
+// Be sure to connect to DB before server starts
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Database connection successful.');
+    return server.listen({ port: 8005 })
+  })
   .then(res => console.log(`Server is running on ${res.url}`))
+  .catch(error => {
+    console.log(`Error: ${error.message} occurs.`)
+  });
