eaf8dba6a4e87931c258bcaf8c9333809accd134
wptechprodigy <waheed.freelancer16@gmail.com>
2019-12-17 11:51:53 +0100

    middleware to handle auth

---
+++ utils/check_auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server');
+const { SECRET_KEY } = require('../config');
+
+module.exports = (context) => {
+  // context = { ... headers }
+  const authHeader = context.req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split('Bearer ')[1];
+
+    if (!token) {
+      throw new AuthenticationError('Invalid/Expired token.');
+    }
+
+    try {
+      // authHeader = [Bearer token]
+      const user = jwt.verify(token, SECRET_KEY);
+      return user;
+    } catch (error) {
+      throw new AuthenticationError('Authentication token must be \'Bearer [token].');
+    }
+  }
+  throw new AuthenticationError('Authorization header must be provided.');
+}
