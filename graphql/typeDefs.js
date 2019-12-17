01a203cd9788c502d778432e04e2146e1648c43a
wptechprodigy <waheed.freelancer16@gmail.com>
2019-12-17 14:46:16 +0100

    add type for comment

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,6 +7,14 @@ module.exports = gql`
     body: String!
     username: String!
     createdAt: String!
+    comment: [Comment]!
+  }
+
+  type Comment {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
   }
 
   type User {
@@ -34,5 +42,6 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
   }
 `;
