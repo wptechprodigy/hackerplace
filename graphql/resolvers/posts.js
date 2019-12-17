c2da93fa72f9ab51d184d63ebfb6e03726003caf
wptechprodigy <waheed.freelancer16@gmail.com>
2019-12-17 11:53:03 +0100

    add logic to get a single post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,44 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../utils/check_auth')
 
 module.exports = {
   Query: {
-    async getPosts () {
+    async getPosts() {
       try {
-        const allPosts = await Post.find();
+        const allPosts = await Post.find().sort({ createdAt: -1 });
         return allPosts;
       } catch (error) {
         throw new Error(error);
       }
     },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (!post) {
+          throw new Error('Post not found.')
+        }
+
+        return post;
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      console.log(user);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    }
   }
 }
