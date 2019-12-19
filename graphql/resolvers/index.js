091306db8f554a1c91b2c2f4bea00a4e8968d9c4
wptechprodigy <waheed.freelancer16@gmail.com>
2019-12-19 06:52:54 +0100

    restructure code for readability

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 const postsResolvers = require('./posts');
 const usersResolvers = require('./users');
 const commentsResolvers = require('./comments');
+
 module.exports = {
   Query: {
     ...postsResolvers.Query,
