edce71b854ddf6e5eb42d842596906662f8ef623
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-17 08:39:58 +0100

    swetup menu

---
+++ client/src/components/NavMenu.tsx
@@ -0,0 +1,49 @@
+import React, { useState, MouseEvent } from 'react';
+import { Menu, MenuItemProps } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+function NavMenu() {
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substr(1);
+
+  const [activeItem, setActiveItem] = useState(path);
+
+  const handleItemClick = (
+    _e: MouseEvent,
+    { name }: MenuItemProps,
+  ) => {
+    if (name) {
+      setActiveItem(name);
+    }
+  };
+
+  return (
+    <Menu pointing secondary>
+      <Menu.Item
+        name="home"
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={Link}
+        to={'/'}
+      />
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="login"
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+          as={Link}
+          to={'/login'}
+        />
+        <Menu.Item
+          name="register"
+          active={activeItem === 'register'}
+          onClick={handleItemClick}
+          as={Link}
+          to={'/register'}
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+}
+
+export default NavMenu;
