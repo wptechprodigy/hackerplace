69405e904b074795ea39c6ad25a80bb3804e6dc7
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-17 08:30:01 +0100

    setup page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Home from './pages/Home'
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hackerplace</h1>
-    </div>
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
   );
 }
 
